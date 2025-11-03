bd3c174fec7fa262e2f621e4cb2819bde0b71f7a
nigelpetargue <nigelpetargue@gmail.com>
2025-11-03 21:50:59 +0800

    update resume

--- utils/experience.ts
+++ utils/experience.ts
@@ -14,18 +14,10 @@ export const experience = [
     title: 'Wela Online System',
     position: 'Junior Web Developer(Intern)',
     time: 'August - October 2022',
-    location: 'Ideaspace 172 Salcedo, Legazpi Village, Makati City, Philippines',
+    location:
+      'Ideaspace 172 Salcedo, Legazpi Village, Makati City, Philippines',
     description:
       'Engage in close collaboration with both the design and development teams to foster a cohesive and synergistic workflow that ensures the successful realization of projects.',
     tech: ['React', 'Typescript', 'NextJS', 'Redux', 'Firebase'],
   },
-  {
-    title: 'San Benissa Garden Villa',
-    position: 'Desktop Support Technician(Intern)',
-    time: 'October - December 2017',
-    location: 'G, 0 Commonwealth Ave, Novaliches, Quezon City, Metro Manila',
-    description:
-      ' Provide prompt technical assistance, troubleshooting and  maintaining network infrastructure for seamless operations.  Customer centric approach ensured efficient IT service for resident staff.',
-    tech: ['WordPress', 'Microsoft Excel', 'VBA', 'Networking'],
-  },
-]
+];
